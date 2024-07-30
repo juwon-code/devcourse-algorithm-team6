0135861c6eab107345698be1b61c0a4f780dc8e7
asklas <121223022+asklas@users.noreply.github.com>
2024-07-30 18:50:53 +0900

    Create PGS_덧칠하기.java

---
+++ 임재영/weeks_01/PGS_덧칠하기.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public static int solution(int n, int m, int[] section) {
+        int count = 0;
+        int pr_idx = -1;
+
+        for (int i : section) {
+            if (i > pr_idx) {
+                count++;
+                pr_idx = i + m - 1;
+            }
+        }
+
+        return count;
+    }
+}
